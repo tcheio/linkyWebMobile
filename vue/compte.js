596ab6ee176a6a5c5353707017a576225b06d076
tcheio <thomas.cheio0105@gmail.com>
2024-04-29 10:07:49 +0200

    ajout du formulaire template

--- vue/compte.js
+++ vue/compte.js
@@ -1,19 +1,27 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 // État global pour déterminer si l'utilisateur est connecté ou non
 let isUserLoggedIn = null;
 
 function CompteScreen() {
   const [isLoggedIn, setIsLoggedIn] = useState(isUserLoggedIn);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  // Fonction pour simuler la connexion de l'utilisateur
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-    isUserLoggedIn = true; // Met à jour l'état global
+  // Fonction pour gérer la soumission du formulaire de connexion
+  const handleSubmit = () => {
+    // Ici, vous pouvez implémenter la logique de connexion avec votre backend
+    // Par exemple, vérifier les identifiants dans une base de données
+
+    // Pour cet exemple, nous allons simplement simuler une connexion réussie si les champs ne sont pas vides
+    if (username !== '' && password !== '') {
+      setIsLoggedIn(true);
+      isUserLoggedIn = true; // Met à jour l'état global
+    }
   };
 
-  // Fonction pour simuler la déconnexion de l'utilisateur
+  // Fonction pour gérer la déconnexion de l'utilisateur
   const handleLogout = () => {
     setIsLoggedIn(false);
     isUserLoggedIn = false; // Met à jour l'état global
@@ -21,14 +29,25 @@ function CompteScreen() {
 
   return (
     <View style={styles.container}>
-      {isLoggedIn === null ? ( // Vérifie si l'utilisateur est connecté
-        // Affiche le formulaire de connexion si l'utilisateur n'est pas connecté
+      {isLoggedIn === null ? (
         <View>
           <Text>Veuillez vous connecter</Text>
-          <Button title="Se connecter" onPress={handleLogin} />
+          <TextInput
+            style={styles.input}
+            placeholder="Nom d'utilisateur ou Email"
+            onChangeText={text => setUsername(text)}
+            value={username}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Mot de passe"
+            onChangeText={text => setPassword(text)}
+            value={password}
+            secureTextEntry // Masque le texte saisi
+          />
+          <Button title="Se connecter" onPress={handleSubmit} />
         </View>
       ) : (
-        // Affiche les informations du compte si l'utilisateur est connecté
         <View>
           <Text>Informations du compte</Text>
           <Button title="Se déconnecter" onPress={handleLogout} />
@@ -43,6 +62,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  input: {
+    height: 40,
+    width: 250,
+    margin: 10,
+    borderWidth: 1,
+    paddingHorizontal: 10,
   },
 });
 
